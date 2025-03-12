77c03488f69356fe2ad89a60f64061622096a9c0
Josue67l <josueguerrero85314@gmail.com>
2025-03-12 11:15:09 -0400

    Add files via upload

---
+++ números1-30.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Números1_30
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Números impares del 1 al 30:");
+
+            for (int i = 1; i <= 30; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
