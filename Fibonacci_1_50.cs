b8df0a5cc05775a8781a76f9ea62df4986795640
Josue67l <josueguerrero85314@gmail.com>
2025-03-12 11:13:30 -0400

    Add files via upload

---
+++ Fibonacci_1_50.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci1_50
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Serie de Fibonacci hasta 50:");
+
+            int a = 0, b = 1, siguiente;
+
+            Console.Write(a + " " + b + " ");
+
+            siguiente = a + b;
+
+            while (siguiente <= 50)
+            {
+                Console.Write(siguiente + " ");
+                a = b;
+                b = siguiente;
+                siguiente = a + b;
+            }
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ formula_general.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Fórmula_general
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Solución de ecuaciones cuadráticas usando la fórmula general");
+
+            Console.Write("Ingrese el valor de a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el valor de c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double discriminante = (b * b) - (4 * a * c);
+
+            if (discriminante > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
+                Console.WriteLine($"Las soluciones son: x1 = {x1}, x2 = {x2}");
+            }
+            else if (discriminante == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine($"La ecuación tiene una única solución: x = {x}");
+            }
+            else
+            {
+                Console.WriteLine("La ecuación no tiene soluciones reales (las raíces son complejas).");
+            }
+
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
