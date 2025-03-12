806b9e5419c045e1cbc0eda3018b03c53ff56aeb
Josue67l <josueguerrero85314@gmail.com>
2025-03-12 11:10:39 -0400

    Add files via upload

---
+++ multiplicar_dos_numeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Multiplicación de dos números");
+        Console.Write("Ingresa el primer número:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingresa el segundo número:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double resultado = num1 * num2;
+        Console.WriteLine($"El resultado de {num1} x {num2} es: {resultado}");
+        Console.WriteLine("Presione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
+
---
+++ primos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Números_primos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Verificar si un número es primo");
+
+            Console.Write("Ingrese un número entero positivo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (EsPrimo(numero))
+            {
+                Console.WriteLine($"{numero} es un número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} NO es un número primo.");
+            }
+
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+        static bool EsPrimo(int num)
+        {
+            if (num < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
+
